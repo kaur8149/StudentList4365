0ace5775996e8dd7dad5b18e734963bca8c46ea3
kaur8149 <kaur8149@sheridancollege.ca>
2020-01-23 10:19:54 -0700

    This is for PartTimeStudent

---
+++ src/PartTimeStudent.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * This class represent a part-time  student.
+ * @author Admin
+ */
+public class extends PartTimeStudent {
+    private int numCourses;
+    public PartTimeStudent(String name, int numCourses){
+        super(name);
+        this.numCourses=numCourses;
+        
+    }
+    public void set
+    
+}
